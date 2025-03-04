b3187f243645b065d1af948be656d8a50e87723b
https-dana <bogdana.kolesnyk@gmail.com>
2025-03-04 20:43:41 +0200

    Initialize pygame and tkinter

---
+++ arckanoid.py
@@ -0,0 +1,10 @@
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+from random import randrange as rnd
+
+pygame.init()
+pygame.font.init()
+
+root = tk.Tk()
+root.withdraw()  # Приховуємо головне вікно Tkinter
