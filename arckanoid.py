ccff9b8df55d926486288c5e82b59a22db14c431
ruzxkovam18@gmail.com <ruzxkovam18@gmail.com>
2025-03-10 18:26:17 +0200

    Implement ball physics

--- arckanoid.py
+++ arckanoid.py
@@ -20,3 +20,12 @@ paddle = pygame.Rect(WIDTH // 2 - paddle_w // 2, HEIGHT - paddle_h - 10, paddle_
 for event in pygame.event.get():
     if event.type == pygame.MOUSEMOTION:
         paddle.centerx = event.pos[0]  # Рух платформи мишею
+
+ball_radius = 20
+ball_speed = 6
+ball_rect = int(ball_radius * 2 ** 0.5)
+ball = pygame.Rect(rnd(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
+
+ball.x += ball_speed * dx
+ball.y += ball_speed * dy
